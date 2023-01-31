b3d7d14eae8afabe68aca77ae093d898123eb461
kishan-ateerna <76469398+Tusif-Hassan-Rizvi@users.noreply.github.com>
2023-01-31 23:02:35 +0530

    Update
    Added quick search logic

--- components/Mainbox.js
+++ components/Mainbox.js
@@ -4,6 +4,7 @@ import Loadder from "./Loadder";
 
 function Mainbox(props) {
   const [indices, setIndices] = useState("");
+  const [breifdata, setBreifdata] = useState([]);
 
   const OnclickIndices = (e) => {
     setIndices(e.target.innerText);
@@ -15,7 +16,8 @@ function Mainbox(props) {
   // implementing search prefrence
   useEffect(() => {
     let showinfo = document.getElementById("ShowInfo");
-    let ParaDisplay=document.getElementById("para");
+    let ParaDisplay = document.getElementById("para");
+    let SearchAll = document.getElementById("searchData");
     let value = props.searchvalue;
     value = value.toUpperCase();
     let noResults = true;
@@ -29,20 +31,89 @@ function Mainbox(props) {
         child.style.display = "none";
       }
     }
-   noResults?ParaDisplay.style.display="block":ParaDisplay.style.display="none";
+    noResults
+      ? (ParaDisplay.style.display = "block")
+      : (ParaDisplay.style.display = "none");
     ParaDisplay.innerHTML = noResults ? "Not found!" : "";
+
+// search all logic 
+if(SearchAll.children[0]){
+  console.log(SearchAll.children[0])
+  SearchAll.children[0].style.display="flex"
+}
   }, [props.searchvalue]);
 
   useEffect(() => {
     let paraDisplay = document.getElementById("para");
-    paraDisplay.style.display='none'
+    paraDisplay.style.display = "none";
   }, [indices]);
 
-
-
+  useEffect(() => {
+    fetch("http://localhost:3000/api/alldata")
+      .then((response) => response.json())
+      .then((response) => {
+        setBreifdata(response);
+      });
+    console.log("call");
+  }, []);
 
   return (
     <>
+      <div id="searchData">
+        {breifdata.length === 0
+          ? null
+          : breifdata.map(
+              (item, index) =>
+              props.searchvalue!=="" &&(item.symbol.includes(props.searchvalue.toUpperCase()) ? 
+              <div key={index} className={styles.showdata} style={{ display: "none", boxShadow:"none", border:"none", alignItems:"center", justifyContent:"center" }}>        
+                    <span className={styles.symbol}>
+                      <div className={styles.infotext}>COMPANY NAME</div>
+                      {item.symbol}
+                    </span>
+                    <span className={styles.open} id="open">
+                      <div className={styles.infotext}>OPEN</div>
+                      {item.open}
+                    </span>
+                    <span className={styles.dayHigh}>
+                      <div className={styles.infotext}>DAY HIGH</div>
+                      {item.dayHigh}
+                    </span>
+                    <span className={styles.dayLow}>
+                      <div className={styles.infotext}>DAY LOW</div>
+                      {item.dayLow}
+                    </span>
+                    <span className={styles.lastPrice} id="lastPrice">
+                      <div className={styles.infotext}>LAST PRICE</div>
+                      {item.lastPrice}
+                    </span>
+                    <span className={styles.change}>
+                      <div className={styles.infotext}>PRICE CHANGE</div>
+                      <span
+                        id="pricechange"
+                        style={
+                          item.change >= 0
+                            ? { color: "green" }
+                            : { color: "red" }
+                        }
+                      >
+                        {item.change.toFixed(2)}
+                      </span>
+                    </span>
+                    <span className={styles.pChange} id="pc">
+                      <div className={styles.infotext}>PROFIT/LOSS</div>
+                      <span
+                        style={
+                          item.pChange >= 0
+                            ? { color: "green" }
+                            : { color: "red" }
+                        }
+                      >
+                        {item.pChange.toFixed(2)}%
+                      </span>
+                    </span>
+              </div> : null
+            ))}
+      </div>
       <div className={styles.heading}>TODAY INSIGHTS</div>
 
       {/* stock list */}
@@ -109,11 +180,28 @@ function Mainbox(props) {
                     </span>
                     <span className={styles.change}>
                       <div className={styles.infotext}>PRICE CHANGE</div>
-                      <span id="pricechange" style={item.change>=0? {color:"green"}:{color:"red"}}>{item.change.toFixed(2)}</span>
+                      <span
+                        id="pricechange"
+                        style={
+                          item.change >= 0
+                            ? { color: "green" }
+                            : { color: "red" }
+                        }
+                      >
+                        {item.change.toFixed(2)}
+                      </span>
                     </span>
                     <span className={styles.pChange} id="pc">
                       <div className={styles.infotext}>PROFIT/LOSS</div>
-                      <span style={item.pChange>=0? {color:"green"}:{color:"red"}}>{item.pChange.toFixed(2)}%</span>
+                      <span
+                        style={
+                          item.pChange >= 0
+                            ? { color: "green" }
+                            : { color: "red" }
+                        }
+                      >
+                        {item.pChange.toFixed(2)}%
+                      </span>
                     </span>
                   </div>
                 ))
