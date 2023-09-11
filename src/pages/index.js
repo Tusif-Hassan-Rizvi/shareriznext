0d224d26a0b8aac4b96f7aed49b13bfc4238f456
Tusif-Hassan-Rizvi <tusifhassanrizvi@gmail.com>
2023-09-11 16:27:21 +0530

    fixed api issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from "react";
-import Head from "next/head";
-import Image from "next/image";
-import axios from "axios";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Index.module.css";
-import Header from "components/Header";
-import Footer from "components/Footer";
-import Search from "components/Search";
-import Mainbox from "components/Mainbox";
-import About from "components/About";
-import Stockfulldata from "components/Stockfulldata";
+import React, { useState, useEffect } from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import axios from 'axios';
+import { Inter } from '@next/font/google';
+import styles from '@/styles/Index.module.css';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+import Search from 'components/Search';
+import Mainbox from 'components/Mainbox';
+import About from 'components/About';
+import Stockfulldata from 'components/Stockfulldata';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export async function getServerSideProps() {
-  const indices = "NIFTY 50";
+  const indices = 'NIFTY 50';
   const link1 = `${process.env.NEXT_PUBLIC_INDICES}?indices=${indices}`;
 
   // fetch data from for indices
@@ -22,7 +22,7 @@ export async function getServerSideProps() {
   const IndicesData = firstApiResponse.data;
 
   // Fetch data for all data
-  const AllData = { message: "not found" };
+  const AllData = { message: 'not found' };
 
   return {
     props: {
@@ -33,8 +33,8 @@ export async function getServerSideProps() {
 }
 
 export default function Home(props) {
-  const [searchvalue, setSearchvalue] = useState("");
-  const [indices, setIndices] = useState("NIFTY 50");
+  const [searchvalue, setSearchvalue] = useState('');
+  const [indices, setIndices] = useState('NIFTY 50');
   const [stockdata, setStockdata] = useState(props.IndicesData);
   const [loading, setLoading] = useState(false);
 
@@ -65,7 +65,7 @@ export default function Home(props) {
         setStockdata(response);
       })
       .catch(() => {
-        setStockdata({ message: "Data not found!" });
+        setStockdata({ message: 'Data not found!' });
       });
   }, [indices]);
   return (
@@ -88,21 +88,27 @@ export default function Home(props) {
           stockfulldata={stockfulldata}
         />
       )}
-      <section style={stockfulldata?{display:'none'}:{display:'block'}}>
-      <Header></Header>
-      <About></About>
-      <Search SearchValue={SearchValue} ALLDATA={props.AllData}  FullDetails={FullDetails}
-        stockfulldata={stockfulldata}></Search>
-      <Mainbox
-        Indices={GetIndices}
-        stockdata={stockdata}
-        loading={loading}
-        searchvalue={searchvalue}
-        FullDetails={FullDetails}
-        stockfulldata={stockfulldata}
+      <section
+        style={stockfulldata ? { display: 'none' } : { display: 'block' }}
+      >
+        <Header></Header>
+        <About></About>
+        <Search
+          SearchValue={SearchValue}
+          ALLDATA={props.AllData}
+          FullDetails={FullDetails}
+          stockfulldata={stockfulldata}
+        ></Search>
+        <Mainbox
+          Indices={GetIndices}
+          stockdata={stockdata}
+          loading={loading}
+          searchvalue={searchvalue}
+          FullDetails={FullDetails}
+          stockfulldata={stockfulldata}
         ></Mainbox>
-      <Footer></Footer>
-        </section>
+        <Footer></Footer>
+      </section>
     </>
   );
 }
